941693b33af74900c37ab51ab7c8a8b74644d011
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-10-02 12:29:17 -0400

    adding transactions and users

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -4,6 +4,9 @@ from .views import *
 
 
 urlpatterns = [
+    path('users/',UserViewSet.as_view({'post': 'create'}),name="create_user"),
     path('users/',UserViewSet.as_view({'get': 'list_users'}),name="users_all"),
+        
+    path('transactions/',TransactionsViewSet.as_view({'post':'create'}), name="create_trans"),
     path('transactions/<username>',TransactionsViewSet.as_view({'get':'retrieve'}),name="user_trans")
 ]
--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -3,18 +3,43 @@ from .models import *
 # Create your views here.
 from django.shortcuts import get_object_or_404
 from .serializers import *
-from rest_framework import viewsets,response
+from rest_framework import viewsets,response, status
 
 class UserViewSet(viewsets.ViewSet):
 
     def list_users(self, request):
         queryset = StockUser.objects.all()
         serializer = StockUserSerializer(queryset, many=True)
-        return response.Response(serializer.data)
+        return response.Response(serializer.data,status=status.HTTP_202_ACCEPTED) 
+
+
+    def create(self, request):
+        #assume that we cannot have dupplicate users
+        serializer = StockUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return response.Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
+
+    
 
 
 
 class TransactionsViewSet(viewsets.ViewSet):
+
     def retrieve(self,request,username=None):
         queryset = StockUser.objects.all()
         user = get_object_or_404(queryset, userID=username)
@@ -23,3 +48,25 @@ class TransactionsViewSet(viewsets.ViewSet):
         
         
         return response.Response(serializer.data)
+    
+    def create(self, request):
+        serializer = TransactionsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return response.Response(status=status.HTTP_400_BAD_REQUEST)
+
+    
+
+    def destroy(self, request, pk=None):
+        pass
+    
+class StockViewSet(viewsets.ViewSet):
+    def create(self, request):
+        serializer = StockSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return response.Response(status=status.HTTP_400_BAD_REQUEST)

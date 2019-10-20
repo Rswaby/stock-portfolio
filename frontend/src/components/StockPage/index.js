5ff0b51d1eba4462781ba816c2b192bc06853538
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-10-20 16:33:36 -0400

    adding grid setup for stock details

--- frontend/src/components/StockPage/index.js
+++ frontend/src/components/StockPage/index.js
@@ -4,14 +4,18 @@ import React, { Component } from 'react';
 
 
 class StockPage extends Component {
-    
-    componentWillMount(){
+
+    componentWillMount() {
         console.log(this.props)
     }
     render() {
         return (
             <div className={"stock-detail-wrapper"}>
-            
+                <div className={"stock-title"}>Stock-title-page</div>
+                <div className={"stock-details-section"}>
+                    <div className={"stock-info-section"}>Stock-detail</div>
+                    <div className={"stock-purchase-or-login"}> Login or purchase Stock section</div>
+                </div>
             </div>
         )
     }

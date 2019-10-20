4f37839d030479daa9452e091ceef6a8a8ceb069
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-10-20 12:34:24 -0400

    cards

--- frontend/src/components/StockCard/index.js
+++ frontend/src/components/StockCard/index.js
@@ -3,8 +3,11 @@ import React from 'react';
 const StockCard = ({ data }) => (
     <div className={"card"}>
         <div className={"content"}>
-           <h6> {data["1. symbol"]}</h6>
-           <p>{data["2. name"]}</p>
+            <h6> {data["1. symbol"]}</h6>
+            <p>{data["2. name"]}</p>
+            <br />
+            <p>Market Open : {data["5. marketOpen"]} </p>
+            <p>Market Close : {data["6. marketClose"]}</p>
         </div>
     </div>
 )

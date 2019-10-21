554f84b17da224c6274bdeee7bc820cd3b56b70b
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-10-21 00:14:53 -0400

    auth condition

--- frontend/src/components/StockPage/index.js
+++ frontend/src/components/StockPage/index.js
@@ -1,32 +1,81 @@
 import React, { Component } from 'react';
-// import { AuthUserContext } from '../Session';
-
-
+import axios from 'axios';
+import { AuthUserContext } from '../Session';
+import { Link } from 'react-router-dom';
+const style = {
+    link: {
+      "textDecoration": "none",
+      "color": "black",
+      "textAlign": "center",
+    }
+  }
 
 class StockPage extends Component {
     state = {
-        symbol: ''
+        symbol: '',
+        loaded: '',
+        data: {}
     }
     componentWillMount() {
         const { symbol } = this.props.match.params;
         this.setState({
             symbol: symbol
         })
+
+        if (symbol) {
+            this.fetchStockInfo(symbol)
+        }
+
         console.log(this.props)
+    }
+
+    handlePurchase = () => {
+
+    }
+
+    fetchStockInfo = (symbol) => {
+
+        axios.get(`/api/live/${symbol}/daily`)
+            .then(res => {
+                const data = res.data
+                this.setState({
+                    data: data
+                })
+            })
+
     }
 
 
     render() {
-        const { symbol } = this.state;
+        const { symbol, data } = this.state;
+        console.log(data.data)
         return (
-            <div className={"stock-detail-wrapper"}>
-                <div className={"stock-title"}>{localStorage.getItem(symbol)}</div>
+            <div className={"m-top-50"}>
+                <h5 className={"stock-title"}>{localStorage.getItem(symbol)}</h5>
                 <div className={"stock-details-section m-left-5"}>
                     <div className={"stock-info-section"}>
-                        Stock-detail
+                        <h6> Symbol : {symbol}</h6>
+                        <br />
+                        {data.data && (<div>
+                            <p>Open   : ${data.data["1. open"]}</p>
+                            <p>Close  : ${data.data["4. close"]}</p>
+                            <p>High   : ${data.data["2. high"]}</p>
+                            <p>Low    : ${data.data["3. low"]}</p>
+                            <p>Volume : {data.data["5. volume"]}</p>
+                        </div>
+                        )}
                     </div>
                     <div className={"stock-purchase-or-login"}>
-                        Login or purchase Stock section
+                        <AuthUserContext.Consumer>
+                            {authUser =>
+                                authUser ? (
+                                    <div>purchase stock</div>
+                                ) : (
+                                    <Link  to={"/signin"}>SIGN IN</Link>
+                                )
+
+                            }
+                        </AuthUserContext.Consumer>
                     </div>
                 </div>
             </div>

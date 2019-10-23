30a56f5098e4a86676ccf43be6bf7cc0e14505a3
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-10-23 02:29:37 -0400

    heroku procfile :

--- backend/stockapp/settings.py
+++ backend/stockapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,5 +126,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+if "TRAVIS" not in os.environ:
+    django_heroku.settings(locals())
 
 STATIC_URL = '/static/'

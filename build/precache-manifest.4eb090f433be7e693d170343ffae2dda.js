4059333a56a96ec7db6d12ae9209f5aefc8970db
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-10-26 12:03:55 -0400

    build

---
+++ build/precache-manifest.4eb090f433be7e693d170343ffae2dda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9ce2d7d17a56309d3f22777d35a802",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cae4d5a47f15c1ffd020",
+    "url": "/static/css/main.0bd087d1.chunk.css"
+  },
+  {
+    "revision": "48897aabb6cb29f5da74",
+    "url": "/static/js/2.06eed015.chunk.js"
+  },
+  {
+    "revision": "cae4d5a47f15c1ffd020",
+    "url": "/static/js/main.7a074711.chunk.js"
+  },
+  {
+    "revision": "08468efb4defab2e6123",
+    "url": "/static/js/runtime-main.7332a7da.js"
+  },
+  {
+    "revision": "16cfe85bb15aa5289ee879792a825d26",
+    "url": "/static/media/cool-background.16cfe85b.png"
+  }
+]);
